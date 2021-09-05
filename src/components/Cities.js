fb048e4c839f70b44e5d611e85aece64b49e4be8
Mallikarjun Ople <arjunople@gmail.com>
2021-09-05 22:11:13 +0530

    City section updated

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -3,34 +3,32 @@ import "./css/main.css";
 import { NavLink } from "react-router-dom";
 import { cityData } from "./city/cityData";
 
-const Cities=()=>{
-
-
-const cityCard=({ cityName, cityDescription, cityPath })=>{
-  return(
-  <>
-    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 project wow animated animated4 fadeInLeft">
-      <div className="project-hover">
-        <h2 className="text-uppercase">{cityName}</h2>
-        <hr />
-        <p>{cityDescription}</p>
-        <NavLink to={`/citypage/${cityPath}`}>Explore</NavLink>
-      </div>
-    </div>
-  </>
-  );
-}
+const Cities = () => {
+  // const cityCard=({ cityName, cityDescription, cityPath })=>{
+  //   return(
+  //   <>
+  //     <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 project wow animated animated4 fadeInLeft">
+  //       <div className="project-hover">
+  //         <h2 className="text-uppercase">{cityName}</h2>
+  //         <hr />
+  //         <p>{cityDescription}</p>
+  //         <NavLink to={`/citypage/${cityPath}`}>Explore</NavLink>
+  //       </div>
+  //     </div>
+  //   </>
+  //   );
+  // }
   return (
     <>
       <div className="container-fluid my-5">
         <div className="title-style">Popular Cities</div>
         {/* {cityCard(cityData.delhi,cityData.desc2,cityData.delhi)} */}
-        <cityCard
+        {/* <cityCard
           cityName={cityData.delhi}
           cityDescription={cityData.desc1}
           cityPath={cityData.delhi}
         />
-        {/* <cityCard
+         <cityCard
           cityName={cityData.mumbai}
           cityDescription={cityData.desc2}
           cityPath={cityData.mumbai}
@@ -46,36 +44,43 @@ const cityCard=({ cityName, cityDescription, cityPath })=>{
           cityPath={cityData.banglore}
         /> */}
 
-        {/* <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 project project-2 wow animated animated3 fadeInLeft">
+        <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 project wow animated animated4 fadeInLeft">
           <div className="project-hover">
-            <h2>{mumbai}</h2>
+            <h2 className="text-uppercase">{cityData.delhi}</h2>
             <hr />
-            <p>{desc1}</p>
-            <NavLink to={`/citypage/${mumbai}`}>Explore</NavLink>
+            <p>{cityData.desc1}</p>
+            <NavLink to={`/citypage/${cityData.delhi}`}>Explore</NavLink>
+          </div>
+        </div>
+        <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 project project-2 wow animated animated3 fadeInLeft">
+          <div className="project-hover">
+            <h2>{cityData.mumbai}</h2>
+            <hr />
+            <p>{cityData.desc2}</p>
+            <NavLink to={`/citypage/${cityData.mumbai}`}>Explore</NavLink>
           </div>
         </div>
         <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 project project-3 wow animated animated2 fadeInLeft">
           <div className="project-hover">
-            <h2>Pune</h2>
+            <h2>{cityData.pune}</h2>
             <hr />
-            <p>{desc3}</p>
-            <NavLink to={`/citypage/${pune}`}>Explore</NavLink>
+            <p>{cityData.desc3}</p>
+            <NavLink to={`/citypage/${cityData.pune}`}>Explore</NavLink>
           </div>
         </div>
 
         <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 project project-4 wow animated fadeInLeft">
           <div className="project-hover">
-            <h2>Banglore</h2>
+            <h2>{cityData.banglore}</h2>
             <hr />
-            <p>{desc4}</p>
-            <NavLink to={`/citypage/${banglore}`}>Explore</NavLink>
+            <p>{cityData.desc4}</p>
+            <NavLink to={`/citypage/${cityData.banglore}`}>Explore</NavLink>
           </div>
-        </div> */}
+        </div>
         <div className="clearfix"></div>
       </div>
     </>
   );
-}
-
+};
 
 export default Cities;
